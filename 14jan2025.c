e64201a4b1f030d3c2e3dc102427d13a111b4cd5
Shivam Negi <toradora7703@gmail.com>
2025-01-14 22:04:33 +0530

    Add files via upload

---
+++ 14jan2025.c
@@ -0,0 +1,24 @@
+//  Print Hello world
+/*
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+    return 0;
+}
+*/
+
+
+// take 2 numbers from user and print their sum
+/*
+ #include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter 2 numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Sum of %d and %d is %d\n", a, b, a+b);
+    return 0;
+}
+*/
+
